22774595e211041794dbccaab70a7704fd88871d
jhoset ceron <jhosetceron@MacBook-Pro.lan>
2020-09-30 12:13:57 -0400

    09/30/20

---
+++ services.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <link href="css/media.css" rel="stylesheet" />
+        <link href="css/main.css" rel="stylesheet" />
+        <!-- CSS only -->
+        <title>LeeBoyz Developerz</title>     
+    </head>
+    <body>
+        <header>
+            <div class="the-topnav">
+                <?php include("include/header.html"); ?>
+            </div>
+        </header>
+        <div id="wrapper">
+            <div class="services-content">
+                <div class="photography">
+                    <p>content</p>
+                </div>
+                <div class="domain-services">
+                    <p>content</p>
+                </div>
+                <div class="websites">
+                    <p>content</p>
+                </div>
+            </div>
+        </div>
+        <footer class="main-footer">
+            <?php include("include/footer.html") ?>
+        </footer>
+    </body>
+</html>
